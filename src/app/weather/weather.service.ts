0acfbc889685e40d784e2195c1933e83839cbb5d
Camilaebf <camilaebf@gmail.com>
2020-12-17 17:50:38 -0300

    add: weather service

---
+++ src/app/weather/weather.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class WeatherService {
+
+  constructor(private httpClient: HttpClient) { }
+}
